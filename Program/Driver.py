693fa6a3048f2fe07f35ed2c344b94d94563c6cf
Nick Kokenis <37492342+nkokenis@users.noreply.github.com>
2021-01-08 09:13:19 -0800

    Update 1/8/2021

--- Program/Driver.py
+++ Program/Driver.py
@@ -1,14 +1,12 @@
 from Node import Node
 from Graph import Graph
 class Driver:
-    #gather input for parameters of the graph
     size = int(input("Enter the size of the graph: "))
     startX = int(input("Enter the x value of the start node: "))
     startY = int(input("Enter the y value of the start node: "))
     endX = int(input("Enter the x value of the end node: "))
     endY = int(input("Enter the y value of the end node: "))
 
-    #initialize the graph
-    graph = Graph(size, startX, startY, endX, endY)
+    graph = Graph(size,startX,startY,endX,endY)
     graph.displayGraph()
     graph.findShortestPath()
