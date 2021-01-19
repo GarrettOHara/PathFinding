b9110bf2cb72fec6f5361efbf867fdcc71fb9d97
Nick Kokenis <37492342+nkokenis@users.noreply.github.com>
2021-01-19 09:51:12 -0800

    Update to oldFiles

--- Program/oldFiles/Graph.py
+++ Program/oldFiles/Graph.py
@@ -1,68 +1,98 @@
 from Node import Node
 import math
+
 class Graph:
-    def __init__(self, size, startX, startY, endX, endY):
-        size = int(size)
-        self.graph = [[Node(-1, -1, "none") for y in range(size)] for x in range(size)]
-        self.startNode = Node(startX,startY,"start")
-        self.endNode = Node(endX,endY,"end")
-
-    def findHeuristic(self, currNode):
-        x1 = currNode.xPos
-        x2 = Node.endNode.xPos
-        y1 = currNode.yPos
-        y2 = Node.endNode.yPos
-        xDifference = x2 - x1
-        yDifference = y2 - y1
-        currNode.heuristic = math.sqrt((int(math.pow(xDifference, 2))) + (int(math.pow(yDifference, 2))))
+    def __init__(self,size,startX,startY,endX,endY):
+        self.graph = [[Node(x,y,"open") for y in range(size)] for x in range(size)]
+        self.graph[startX][startY].nodeType = "start"
+        self.graph[endX][endY].nodeType = "end"
+        self.startNode = self.graph[startX][startY]
+        self.endNode = self.graph[endX][endY]
+        self.graph[5][5].nodeType = "closed"
     
-    def populate(self):
-        self.startNode.setStartNode(self.startNode)
-        self.endNode.setEndNode(self.endNode)
-        for y in range(len(self.graph)):
-            for x in range(len(self.graph[0])):
-                
-                if((x == (self.startNode.xPos) and y == (self.startNode.yPos)) or \
-                   (x == (self.endNode.xPos) and y == (self.endNode.yPos))):
-                   #account for closed nodes
-                   print("  x  "),
-                   continue
-                else:
-                    self.graph[x][y] = Node(x,y,"open")
-                    self.graph[x][y].heuristic = self.findHeuristic(self.graph[x][y])
-                    self.graph[x][y].setStartNode(self.startNode)
-                    self.graph[x][y].setEndNode(self.endNode)
-                    print("(" + str(x) + "," + str(y) + ")" + " " + str(self.graph[x][y].heuristic) + " " + str(Node.endNode.yPos)),
-            print(" ")
-    
-    
-
-    def getAdjacentNodes(self, node):
-        print(" ")
-        nodes = []
-        nodeX = int(node.xPos)
-        nodeY = int(node.yPos)
-        nodes.append(self.get((nodeX), (nodeY + 1)))
-        nodes.append(self.get((nodeX + 1), (nodeY + 1)))
-        nodes.append(self.get((nodeX + 1), (nodeY)))
-        nodes.append(self.get((nodeX + 1), (nodeY - 1)))
-        nodes.append(self.get((nodeX), (nodeY - 1)))
-        nodes.append(self.get((nodeX - 1), (nodeY - 1)))
-        nodes.append(self.get((nodeX - 1), (nodeY)))
-        nodes.append(self.get((nodeX - 1), (nodeY + 1)))
-        return nodes
-    
-    def get(self, x, y):
-        if(((x in range(len(self.graph))) and (y in range(len(self.graph[0]))))):
-            return self.graph[x][y]
-        else:
-            temp = Node(-1,-1,"closed")
-            return temp
-    
-    def clear(self):
+    def displayGraph(self):
         for x in range(len(self.graph)):
-            for y in range(len(self.graph[0])):
-                if((self.graph[x][y].nodeType == "start") or \
-                   (self.graph[x][y].nodeType == "end")   or \
-                   (self.graph[x][y].nodeType == "closed")):
-                    self.graph[x][y].nodeType = "open"
+            for y in range(len(self.graph[x])):
+                print(self.graph[x][y]),
+            print("")
+
+    def getAdj(self,node):
+        adjNodes = []
+        x = int(node.xPos)
+        y = int(node.yPos)
+        if(self.inRange(x,y+1)):
+            adjNodes.append(self.graph[x][y+1])
+        if(self.inRange(x+1,y+1)):
+            adjNodes.append(self.graph[x+1][y+1])
+        if(self.inRange(x+1,y)):
+            adjNodes.append(self.graph[x+1][y])
+        if(self.inRange(x+1,y-1)):
+            adjNodes.append(self.graph[x+1][y-1])
+        if(self.inRange(x,y-1)):
+            adjNodes.append(self.graph[x][y-1])
+        if(self.inRange(x-1,y-1)):
+            adjNodes.append(self.graph[x-1][y-1])
+        if(self.inRange(x-1,y)):
+            adjNodes.append(self.graph[x-1][y])
+        if(self.inRange(x-1,y+1)):
+            adjNodes.append(self.graph[x-1][y+1])
+
+        for node in adjNodes:
+            print(node),
+        print("")
+
+        return adjNodes
+
+    
+    def inRange(self,x,y):
+        if(((x > -1) and (x < len(self.graph))) and ((y > - 1) and (y < len(self.graph)))):
+            return True
+        else:
+            return False
+    
+    def findShortestPath(self):
+        openList = []
+        closedList = []
+
+        openList.append(self.startNode)
+        count = 0
+
+        while(len(openList) > 0):
+
+            minNode = openList[0]
+            minIndex = 0
+            for i in range(len(openList)):
+                if(minNode.f > openList[i].f):
+                    minNode = openList[i]
+                    minIndex = i
+            
+            openList.pop(minIndex)
+            closedList.append(minNode)
+
+            if(minNode == self.endNode):
+                print("Found the end node")
+                
+                for item in closedList:
+                    print(item)
+                return
+                    
+
+            adjNodes = self.getAdj(minNode)
+
+            for node in adjNodes:
+                if node.nodeType == "closed":
+                    continue
+
+                for closedNode in closedList:
+                    if(node == closedNode):
+                        continue
+
+                node.g = minNode.g + 1
+                node.h = ((node.xPos - self.endNode.xPos)**2) + ((node.yPos - self.endNode.yPos)**2)
+                node.f = node.h + node.g
+
+                for openNode in openList:
+                    if((node == openNode) and (node.g > openNode.g)):
+                        continue
+                
+                openList.append(node)
